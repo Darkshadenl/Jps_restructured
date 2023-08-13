0db00f6c2c627d3d6ed63737cacb6addf621f9b8
Quinten M <quinten@infi.nl>
2023-08-13 16:15:04 +0200

    Commited upload changes

--- committed_upload.sh
+++ committed_upload.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+source .env
 # Check for staged git documents
 echo "Checking for staged git documents..."
 staged_files=$(git diff --cached --name-only)
@@ -15,22 +15,22 @@ done
 # Upload the filtered files to the sftp remote server
 HOST=jpsretail.nl
 USER=jpsretail.nl
-PASSWORD=RBHebgXuVq93
+PASSWORD=PASSWORD
 
-echo -e "\033[0;31mUploading files to the remote server...\033[0m"
-echo 
-echo "Change dir to private"
 cd private || exit
 
-for file in $private_files; do
-    if [[ $file == private/public* ]]; then
-        remote_location="sftp://$HOST/www/"
-    else
-        remote_location="sftp://$HOST/private/"
-    fi
-    echo -e "\033[0;33mUploading\033[0m" "\033[0;31m::\033[0m" $remote_location "${file#private/}"
-    duck --username $USER --password $PASSWORD -e overwrite --upload $remote_location "${file#private/}"
-done
+if [[ ${#private_files} -gt 0 ]]; then
+    echo -e "\033[0;31mUploading files to the remote server...\033[0m"
+    for file in $private_files; do
+        if [[ $file == private/public* ]]; then
+            remote_location="sftp://$HOST/www/"
+        else
+            remote_location="sftp://$HOST/private/"
+        fi
+        echo -e "\033[0;33mUploading\033[0m" "\033[0;31m::\033[0m" $remote_location "${file#private/}"
+        duck --username $USER --password $PASSWORD -e overwrite --upload $remote_location "${file#private/}"
+    done
+fi
 
 echo "Upload complete."
 
