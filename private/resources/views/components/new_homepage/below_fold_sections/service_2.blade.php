dc29bafd866d8da37265b8246d5057236ba714b0
Quinten M <quinten@infi.nl>
2023-08-19 17:06:44 +0200

    fix Commerciële planrealisatie winkelcentra layout

--- private/resources/views/components/new_homepage/below_fold_sections/service_2.blade.php
+++ private/resources/views/components/new_homepage/below_fold_sections/service_2.blade.php
@@ -1,4 +1,4 @@
-<section class="relative pt-36 pb-36 mb-12">
+<section class="relative pt-24 pb-20">
     <div
         class="bottom-auto top-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden -mt-20"
         style="height: 80px;"
@@ -18,20 +18,19 @@
             ></polygon>
         </svg>
     </div>
-    <div class="container mx-auto px-4">
-        <div class="items-center flex flex-wrap">
-            <div class="w-full md:w-4/12 ml-auto mr-auto px-4">
+    <div class="container">
+        <div class="items-center flex flex-wrap justify-center">
+            <div class="w-full md:w-8/12 lg:w-6/12 ml-auto mr-auto px-4">
                 <img
                     alt="citycenter"
                     class="max-w-full rounded-lg shadow-lg"
                     src="{{ asset('storage/img/homepage/centre.jpeg') }}"
                 />
             </div>
-            <div class="w-full md:w-5/12 ml-auto mr-auto px-4">
-                <div class="pt-8 md:pr-12">
+            <div class="w-full md:w-full lg:w-5/12 px-4 sm:px-0 pt-4">
                     <h3 class="text-3xl font-semibold">Commerciële planrealisatie winkelcentra</h3>
                     <p class="mt-4 text-lg leading-relaxed text-gray-600">
-                        Mocht u als winkelcentrumeigenaar van plan zijn om uw winkelcentrum te gaan her ontwikkelen, dan
+                        Mocht u als winkelcentrumeigenaar van plan zijn om uw winkelcentrum te gaan herontwikkelen, dan
                         kunnen wij u adviseren en ondersteunen in het commerciële planrealisatie traject vanaf de
                         initiatiefase tot en met de uitvoeringsfase.
                     </p>
@@ -40,8 +39,6 @@
                         schakel tussen enerzijds de huurder en anderzijds de ontwikkelaar/belegger. Afhankelijk van de
                         omvang van het project/de opdracht, zal er een projectteam worden samengesteld.
                     </p>
-
-                </div>
             </div>
         </div>
     </div>
